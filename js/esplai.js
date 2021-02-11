37f4b10367c28bcac62d78b31d6a846718413f01
Martí <martiram8@gmail.com>
2021-02-11 20:02:33 +0100

    començat pagina monitors i reubicacio server

--- js/esplai.js
+++ js/esplai.js
@@ -2,5 +2,18 @@ $(document).on("scroll",  function () {
     var $nav = $(".navbar");
     $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
 });
+  
+async function getMonitors() {
+    try {
+        const response = await axios.get("https://curso-jedi-web.herokuapp.com/pokemons/");
+        const data = await response.data;
+        return data;
+    } catch (error) {
+        console.log(error); 
+    }
+};
+
+function testLogged() {
     
-    
+}
+  
